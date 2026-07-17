25182a6f72646176905103269b8444c2f2ce3987
vmlak29-rgb <vmlak29@gmail.com>
2026-07-17 15:00:56 +0530

    Create 238-ProductOfArrayExceptSelf

---
+++ 238-ProductOfArrayExceptSelf/238-ProductOfArrayExceptSelf.py
@@ -0,0 +1,17 @@
+# Last updated: 17/07/2026, 15:00:56
+class Solution:
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        answer = [1] * n
+
+        left = 1
+        for i in range(n):
+            answer[i] = left
+            left *= nums[i]
+
+        right = 1
+        for i in range(n - 1, -1, -1):
+            answer[i] *= right
+            right *= nums[i]
+
+        return answer
