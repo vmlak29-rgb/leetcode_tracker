3605537c7313db127cf27c60cdc52438eb751dd8
vmlak29-rgb <vmlak29@gmail.com>
2026-07-17 15:02:13 +0530

    Create 107-BinaryTreeLevelOrderTraversalIi

---
+++ 107-BinaryTreeLevelOrderTraversalIi/107-BinaryTreeLevelOrderTraversalIi.py
@@ -0,0 +1,27 @@
+# Last updated: 17/07/2026, 15:02:13
+from collections import deque
+
+class Solution:
+    def levelOrderBottom(self, root):
+        if not root:
+            return []
+
+        result = []
+        queue = deque([root])
+
+        while queue:
+            level = []
+
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+            result.append(level)
+
+        return result[::-1]
