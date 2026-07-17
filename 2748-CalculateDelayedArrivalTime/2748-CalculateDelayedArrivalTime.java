38369277275ba15aae05096879caf38de0ffd77c
vmlak29-rgb <vmlak29@gmail.com>
2026-07-17 14:59:06 +0530

    Create 2748-CalculateDelayedArrivalTime

---
+++ 2748-CalculateDelayedArrivalTime/2748-CalculateDelayedArrivalTime.java
@@ -0,0 +1,6 @@
+// Last updated: 17/07/2026, 14:59:06
+class Solution {
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        return (arrivalTime + delayedTime) % 24;
+    }
+}
