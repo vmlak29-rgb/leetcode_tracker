f191d389240a50eb388b99cae4231a9ea6fddf45
vmlak29-rgb <vmlak29@gmail.com>
2026-07-17 15:00:43 +0530

    Create 326-PowerOfThree

---
+++ 326-PowerOfThree/326-PowerOfThree.java
@@ -0,0 +1,10 @@
+// Last updated: 17/07/2026, 15:00:43
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0) return false;
+        while (n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
