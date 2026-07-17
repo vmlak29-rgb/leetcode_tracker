046937929660c47a17213296de8a8a1a959b23dc
vmlak29-rgb <vmlak29@gmail.com>
2026-07-17 15:03:17 +0530

    Create 57-InsertInterval

---
+++ 57-InsertInterval/57-InsertInterval.py
@@ -0,0 +1,33 @@
+# Last updated: 17/07/2026, 15:03:16
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        """
+        :type intervals: List[List[int]]
+        :type newInterval: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        i = 0
+        n = len(intervals)
+
+        # Add all intervals before the new interval
+        while i < n and intervals[i][1] < newInterval[0]:
+            result.append(intervals[i])
+            i += 1
+
+        # Merge overlapping intervals
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+
+        # Add the merged interval
+        result.append(newInterval)
+
+        # Add the remaining intervals
+        while i < n:
+            result.append(intervals[i])
+            i += 1
+
+        return result
+        
