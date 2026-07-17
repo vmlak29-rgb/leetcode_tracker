5bd67df82f6d696d7a03707445d4b85387038227
vmlak29-rgb <vmlak29@gmail.com>
2026-07-17 15:00:17 +0530

    Create 608-TreeNode

---
+++ 608-TreeNode/608-TreeNode.sql
@@ -0,0 +1,11 @@
+-- Last updated: 17/07/2026, 15:00:17
+# Write your MySQL query statement below
+SELECT id,
+       CASE
+           WHEN p_id IS NULL THEN 'Root'
+           WHEN id IN (SELECT DISTINCT p_id
+                       FROM Tree
+                       WHERE p_id IS NOT NULL) THEN 'Inner'
+           ELSE 'Leaf'
+       END AS type
+FROM Tree;
