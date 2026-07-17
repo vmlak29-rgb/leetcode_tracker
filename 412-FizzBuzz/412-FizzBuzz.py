80d5038f544adda76eed6e4a3a26859e189e919a
vmlak29-rgb <vmlak29@gmail.com>
2026-07-17 15:00:35 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.py
@@ -0,0 +1,16 @@
+# Last updated: 17/07/2026, 15:00:35
+class Solution:
+    def fizzBuzz(self, n):
+        result = []
+
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+
+        return result
