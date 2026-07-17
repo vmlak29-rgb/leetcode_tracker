6f731f302346bd713cdbb6ce1fc848fcd6cab7fd
vmlak29-rgb <vmlak29@gmail.com>
2026-07-17 15:03:59 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.py
@@ -0,0 +1,12 @@
+# Last updated: 17/07/2026, 15:03:58
+class Solution:
+    def removeElement(self, nums, val):
+        k = 0
+
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+
+        return k
+        
