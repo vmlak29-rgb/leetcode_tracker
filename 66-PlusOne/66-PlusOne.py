1618172fdc579e0b9b071e39a0b8f2dbe1525181
vmlak29-rgb <vmlak29@gmail.com>
2026-07-17 15:03:05 +0530

    Create 66-PlusOne

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,13 @@
+# Last updated: 17/07/2026, 15:03:05
+class Solution:
+    def plusOne(self, digits):
+        n = len(digits)
+
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+
+            digits[i] = 0
+
+        return [1] + digits
